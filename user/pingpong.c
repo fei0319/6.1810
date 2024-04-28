19d0a025800b79a5bb65307ffa74f226eda7dbb7
Fei Pan <pan-fei@outlook.com>
2024-04-28 17:19:44 +0800

    pingpong now will close fd correctly

--- user/pingpong.c
+++ user/pingpong.c
@@ -13,15 +13,26 @@ int main(int argc, char *argv[]) {
     int pid = fork();
     if (pid > 0) {
         char buf[] = {'a'};
+
         write(p[1], buf, 1);
+        close(p[1]);
+
         wait((int *)0);
+
         read(p[0], buf, 1);
+        close(p[0]);
+
         printf("%d: received pong\n", getpid());
     } else {
         char buf[1];
+
         read(p[0], buf, 1);
+        close(p[0]);
+
         printf("%d: received ping\n", getpid());
+
         write(p[1], buf, 1);
+        close(p[1]);
     }
     exit(0);
 }
